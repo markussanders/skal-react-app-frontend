e30383cb4560e58dc7cae5a938a7abbbe048f6ac
xmksz3 <markus.sanderst@gmail.com>
2019-07-26 10:59:43 -0500

    Check in, linking homepage to app

--- skal-client/src/App.js
+++ skal-client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LogInSignUp from './components/LogInSignUp';
+import HomePage from './containers/HomePage';
 
 
 class App extends React.Component {
