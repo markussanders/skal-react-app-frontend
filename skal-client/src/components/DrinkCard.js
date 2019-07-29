e1230ac1275ee4fa9e770b27985d52131403e2e9
xmksz3 <markus.sanderst@gmail.com>
2019-07-29 15:59:28 -0500

    Changing branches

--- skal-client/src/components/DrinkCard.js
+++ skal-client/src/components/DrinkCard.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import DrinkSpecs from './DrinkSpecs';
 
 
 const DrinkCard = props =>  {
-     const {drink} = props;
+    const {drink} = props;
 
-        return (
-            <div className="drink-card">
-                <img className="drink-card-img" src={drink.image_url} alt={drink.name}/>
-                <h4 className="drink-card-name">{drink.name}</h4>
-                <p className="drink-card-desc">{drink.description}</p>
-            </div>
-        )
+    const renderDrinkSpecs =  () => {
+        
+    }   
+
+    return (
+        <div className="drink-card" >
+            <img className="drink-card-img" src={drink.image_url} alt={drink.name}/>
+            <h4 className="drink-card-name">{drink.name}</h4>
+            <p className="drink-card-desc">{drink.description}</p>
+        </div>
+    )
 
 }
 
